fd407aa9c0e21258ccc518f97a6e63bf0ee791e3
Alex Tillier <alexander.tillier@gmail.com>
2026-09-22 17:17:08 -0700

    Update v1_driveTrain.java

--- v1_driveTrain.java
+++ v1_driveTrain.java
@@ -4,4 +4,29 @@
 //
 //  Created by Nikhil Rasiah on 9/22/26.
 //
+// IMPORTS FROM FTC
+package org.firstinspires.ftc.teamcode;
 
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+
+@TeleOp
+public class DriveTrain extends LinearOpMode {
+  // Initialize hardware variables (motors)
+  DcMotor frontLeftMotor = hardwareMap.get(DcMotor.class, "frontLeft");
+  DcMotor backLeftMotor = hardwareMap.get(DcMotor.class, "backLeft");
+  DcMotor frontRightMotor = hardwareMap.get(DcMotor.class, "frontRight");
+  DcMotor backRightMotor = hardwareMap.get(DcMotor.class, "backRight");
+
+  frontLeftMotor.setDirection(DcMotorSimple.Direction.REVERSE);
+  backLeftMotor.setDirection(DcMotorSimple.Direction.REVERSE);
+  frontRightMotor.setDirection(DcMotorSimple.Direction.FORWARD);
+  backRightMotor.setDirection(DcMotorSimple.Direction.FORWARD);
+
+  waitForStart();
+
+  
+  
+}
